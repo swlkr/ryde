c861d24dadc7bd05ff2bea7f88ce6196dc3c1f60
Sean Walker <swlkr.rbl@gmail.com>
2024-02-29 09:54:33 -0700

    prep static files

--- crates/static_files/src/lib.rs
+++ crates/static_files/src/lib.rs
@@ -1,22 +1,2 @@
 pub use ryde_static_files_macros::StaticFiles;
 extern crate self as static_files;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[derive(StaticFiles, Debug, PartialEq)]
-    #[folder("static")]
-    struct StaticFile;
-
-    #[test]
-    fn it_works() {
-        let uri = "/static/test.css";
-        let (content_type, bytes) = StaticFile::get(uri).unwrap();
-        assert_eq!(
-            std::str::from_utf8(&bytes).unwrap(),
-            "/* this is test.css */"
-        );
-        assert_eq!(content_type, "text/css");
-    }
-}
